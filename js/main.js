2cbc3768b4d6b7f8bbbce82780a0b046c5843228
perian <dmperian@gmail.com>
2021-03-22 15:24:52 +0200

    Правки семантики, чтобы тревис не ругался

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ const houseTypes = {
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;   // Максимум не включается, минимум включается
+  return Math.floor(Math.random() * (max - min)) + min; // Максимум не включается, минимум включается
 };
 
 const getRandomArrayElement = (array) => {
